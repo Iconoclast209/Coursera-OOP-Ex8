f0257460007cfa61fa04c3fcea4ea539813ce8bd
asmith <mrpresidentsmith@gmail.com>
2019-05-22 06:06:40 -0700

    Exercise completed.

---
+++ Exercise8-Events/Assets/Invoker.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Invoker : MonoBehaviour
+{
+    int counter = 0;
+    Timer timer;
+    EventOne eventOne;
+    EventTwo eventTwo;
+
+    private void Awake()
+    {
+        eventOne = new EventOne();
+        eventTwo = new EventTwo();
+    }
+
+    void Start()
+    {
+        timer = GetComponent<Timer>();
+        timer.Duration = 1f;
+        timer.Run();
+    }
+
+    void Update()
+    {
+        if(timer.Finished)
+        {
+            eventOne.Invoke();
+            eventTwo.Invoke(counter);
+            timer.Run();
+            counter++;
+        }
+    }
+
+    public void AddNoArgumentListener(UnityAction listener)
+    {
+        eventOne.AddListener(listener);
+    }
+
+    public void AddIntArgumentListener(UnityAction<int> listenerInt)
+    {
+        eventTwo.AddListener(listenerInt);
+    }
+
+}
---
+++ Exercise8-Events/Assets/Listener.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Listener : MonoBehaviour
+{
+    Invoker invoker;
+
+    private void Start()
+    {
+        invoker = FindObjectOfType<Camera>().GetComponent<Invoker>();
+        invoker.AddNoArgumentListener(PrintAMessage);
+        invoker.AddIntArgumentListener(PrintIntMessage);
+    }
+
+    public void PrintAMessage()
+    {
+        Debug.Log("This is a message called by an event handler.");
+    }
+
+    public void PrintIntMessage(int i)
+    {
+        Debug.Log("This is a message called by an event handler with an integer attached:  " + i);
+    }
+}
